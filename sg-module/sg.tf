06391f5becaaee1ddf74c1d65aad1986a01c7ed3
Bhargavi <bhargavimandala@gmail.com>
2023-03-23 16:28:43 +0000

    added sg

--- sg-module/sg.tf
+++ sg-module/sg.tf
@@ -3,13 +3,20 @@ resource "aws_security_group" "sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "ec2 sg"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
   egress {
     from_port   = 0
     to_port     = 0
