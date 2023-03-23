628d91574e53db457e4ff472fa64951b5a89c6ec
Bhargavi <bhargavimandala@gmail.com>
2023-03-23 15:48:28 +0000

    added iam policy

--- iam-module/output.tf
+++ iam-module/output.tf
@@ -1,3 +1,12 @@
-output "iam-profile" {
+output "role_arn" {
+    value = aws_iam_role.iam_role.arn
+}
+
+output "role_name" {
+    value = aws_iam_role.iam_role.name
+  
+}
+
+output "ec2-profile" {
     value = aws_iam_instance_profile.ec2_profile.id
 }
